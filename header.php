b9cf8a9f20e0cbcaf6327001c9499439d4d52e16
kavag <47476128+kavag@users.noreply.github.com>
2019-04-27 10:45:12 +0400

    Update header.php

--- header.php
+++ header.php
@@ -9,33 +9,32 @@
   <meta name="author" content="">
 
   <title>Blog Template for Bootstrap</title>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
   <link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
     <![endif]-->
+  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
   <?php wp_head();?>
 </head>
 
 <body>
 
-  <div class="blog-masthead">
-    <div class="container">
-      <nav class="blog-nav">
-        <a class="blog-nav-item active" href="#">Home</a>
-        <a class="blog-nav-item" href="#">New features</a>
-        <a class="blog-nav-item" href="#">Press</a>
-        <a class="blog-nav-item" href="#">New hires</a>
-        <a class="blog-nav-item" href="#">About</a>
-      </nav>
-    </div>
-  </div>
+ <div class="blog-masthead">
+	<div class="container">
+		<nav class="blog-nav">
+			<a class="blog-nav-item active" href="#">Home</a>
+			<?php wp_list_pages( '&title_li=' ); ?>
+		</nav>
+	</div>
+</div>
 
   <div class="container">
 
     <div class="blog-header">
-      <h1 class="blog-title">The Bootstrap Blog</h1>
-      <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+      <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
     </div>
